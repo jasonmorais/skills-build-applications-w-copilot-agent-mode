f4234bd278561421c760555fa4f5fb7034107043
jasonmorais <120504390+jasonmorais@users.noreply.github.com>
2025-06-24 14:33:13 +0000

    database setup and setting changes

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,32 @@
+from djongo import models
+
+class User(models.Model):
+    _id = models.ObjectIdField()
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=128)
+    # Add other fields as needed
+
+class Team(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField('User', related_name='teams')
+
+class Activity(models.Model):
+    _id = models.ObjectIdField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    activity_type = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    date = models.DateTimeField()
+
+class Leaderboard(models.Model):
+    _id = models.ObjectIdField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    points = models.IntegerField()
+
+class Workout(models.Model):
+    _id = models.ObjectIdField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    workout_type = models.CharField(max_length=50)
+    details = models.JSONField()
+    date = models.DateTimeField()
